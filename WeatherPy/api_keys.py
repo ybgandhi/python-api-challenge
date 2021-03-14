74c0ac109e10b701f395805499845175b8dcc360
ybgandhi <gandhibyash@gmail.com>
2021-03-13 22:35:52 -0500

    weather update

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "1bd2a5c8c4c91ed34327f5a8f79fb4ae"
+weather_api_key = "9f279ee247f401a2fed6c4a1e1d70758"
 
 # Google API Key
 g_key = "AIzaSyDaitMmdez8-84gXeNXolUP36yRWs3iNR4"
