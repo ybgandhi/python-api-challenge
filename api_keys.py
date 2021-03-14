2af65c3ca350e550643c2f7eb4722443bc4fa3f1
ybgandhi <gandhibyash@gmail.com>
2021-03-13 21:26:59 -0500

    Update api_keys

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "1bd2a5c8c4c91ed34327f5a8f79fb4ae"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDaitMmdez8-84gXeNXolUP36yRWs3iNR4"
